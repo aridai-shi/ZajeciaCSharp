0d339c5b23faf5ad27a434198483d3b168a8b5a0
aridai-shi <jakubkaczma5@gmail.com>
2022-05-21 09:44:08 +0200

    Kółko i krzyżyk - wersja AI

---
+++ TicTAIcToe/TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1: Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.winLabel = new System.Windows.Forms.Label();
+            this.historyBox = new System.Windows.Forms.GroupBox();
+            this.historyLabel = new System.Windows.Forms.Label();
+            this.Input.SuspendLayout();
+            this.historyBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Controls.Add(this.button9);
+            this.Input.Controls.Add(this.button8);
+            this.Input.Controls.Add(this.button7);
+            this.Input.Controls.Add(this.button6);
+            this.Input.Controls.Add(this.button5);
+            this.Input.Controls.Add(this.button4);
+            this.Input.Controls.Add(this.button3);
+            this.Input.Controls.Add(this.button2);
+            this.Input.Controls.Add(this.button1);
+            this.Input.Location = new System.Drawing.Point(52, 33);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(584, 376);
+            this.Input.TabIndex = 0;
+            this.Input.TabStop = false;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(333, 221);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(79, 79);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.inputButtonClicked);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(248, 221);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(79, 79);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.inputButtonClicked);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(163, 221);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(79, 79);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.inputButtonClicked);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(333, 136);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(79, 79);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.inputButtonClicked);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(248, 136);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(79, 79);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.inputButtonClicked);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(163, 136);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 79);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.inputButtonClicked);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(333, 51);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 79);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.inputButtonClicked);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(248, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 79);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.inputButtonClicked);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 79);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.inputButtonClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(643, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Teraz kolej gracza:";
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(643, 373);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(100, 35);
+            this.resetButton.TabIndex = 2;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetGame);
+            // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.Location = new System.Drawing.Point(643, 116);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(66, 15);
+            this.winLabel.TabIndex = 3;
+            this.winLabel.Text = "WYGRANE:";
+            // 
+            // historyBox
+            // 
+            this.historyBox.Controls.Add(this.historyLabel);
+            this.historyBox.Location = new System.Drawing.Point(643, 169);
+            this.historyBox.Name = "historyBox";
+            this.historyBox.Size = new System.Drawing.Size(145, 164);
+            this.historyBox.TabIndex = 4;
+            this.historyBox.TabStop = false;
+            this.historyBox.Text = "HISTORIA RUCHÓW";
+            // 
+            // historyLabel
+            // 
+            this.historyLabel.AutoSize = true;
+            this.historyLabel.Location = new System.Drawing.Point(41, 19);
+            this.historyLabel.MaximumSize = new System.Drawing.Size(50, 140);
+            this.historyLabel.Name = "historyLabel";
+            this.historyLabel.Size = new System.Drawing.Size(43, 15);
+            this.historyLabel.TabIndex = 0;
+            this.historyLabel.Text = "X: (0,2)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.historyBox);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Input.ResumeLayout(false);
+            this.historyBox.ResumeLayout(false);
+            this.historyBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GroupBox Input;
+        private Button button1;
+        private Label label1;
+        private Button button9;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button resetButton;
+        private Label winLabel;
+        private GroupBox historyBox;
+        private Label historyLabel;
+    }
+}
+
---
+++ TicTAIcToe/TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        private bool player = true;
+        private int[,] gameState = new int[3, 3];
+        private Button[,] buttonMap = new Button[3, 3];
+        private readonly Tuple<int,int> notFoundTuple = new Tuple<int,int>(4,4);
+        private Tuple<int,int> winCounter = new Tuple<int, int>(0,0);
+        private int moveCounter = 0;
+        private int streak = 0;
+        private bool lastWinner = false;
+        private bool firstGame = true;
+        public Form1()
+        {
+            InitializeComponent();
+            InitGame();
+        }
+        private void InitGame()
+        {
+            if (lastWinner == player)
+            {
+                streak++;
+            }
+            else
+            {
+                streak = 0;
+                lastWinner = player;
+            }
+            if (firstGame) lastWinner = player;
+            string streakString = (streak > 0 && !firstGame) ? $"(STREAK:{streak})" : "";
+            if (player)
+            {
+                winLabel.Text = $"WYGRANE:\nO:{winCounter.Item1} {streakString}  X:{winCounter.Item2}";
+            }
+            else
+            {
+                winLabel.Text = $"WYGRANE:\nO:{winCounter.Item1}  X:{winCounter.Item2} {streakString}";
+            }
+            historyLabel.Text = "";
+            moveCounter = 0;
+            resetButton.Enabled = false;
+            resetButton.Visible = false;
+            player = !player;
+            gameState = new int[3, 3];
+            for (int x = 0; x <= 2; x++)
+            {
+                for (int y = 0; y <= 2; y++)
+                {
+                    gameState[x, y] = 0;
+                }
+            }
+            buttonMap = new Button[3, 3];
+            int i = 0;
+            foreach (Button button in Input.Controls)
+            {
+                buttonMap[i % 3, i / 3] = button;
+                button.Tag = i;
+                button.Text = "";
+                i++;
+            }
+            label1.Text = "Teraz kolej gracza: " + (player ? "O" : "X");
+            if (player) makeAIMove();
+        }
+        private void inputButtonClicked(object sender, EventArgs e)
+        {
+            if (player) return;
+            makeMove(sender as Button);
+        }
+        private void makeMove(Button button)
+        {
+            if (endGame() || (moveCounter >= 10)) return;
+            Tuple<int, int> coords = MapButton(button);
+            if (coords != notFoundTuple)
+            {
+                if (gameState[coords.Item1, coords.Item2] != 0) return;
+                gameState[coords.Item1, coords.Item2] = playerInt();
+                button.Text = playerString();
+            }
+            historyLabel.Text += $"{playerString()}: ({coords.Item1},{coords.Item2}) \n";
+            moveCounter++;
+            if (endGame())
+            {
+                MessageBox.Show("Wygrał gracz " + playerString());
+                historyLabel.Text += "Wygrał gracz " + playerString() + "\n";
+                winCounter = new Tuple<int, int>(winCounter.Item1 + playerBonus(true), winCounter.Item2 + playerBonus(false));
+                resetButton.Enabled = true;
+                resetButton.Visible = true;
+                return;
+            }
+            else if (moveCounter >= 9)
+            {
+                MessageBox.Show("REMIS");
+                historyLabel.Text += "REMIS\n";
+                resetButton.Enabled = true;
+                resetButton.Visible = true;
+                winCounter = new Tuple<int, int>(0, 0);
+                return;
+            }
+            player = !player;
+            label1.Text = "Teraz kolej gracza: " + playerString();
+            if (player) makeAIMove(); // jeśli kolej komputera, daj mu zagrać
+        }
+
+        private void makeAIMove()
+        {
+            if (endGame() || (moveCounter >= 10)) return; // nie wykonuj ruchów po remisie
+            Tuple<int, int> aiMove = AICheckMoves(); // sprawdź gdzie masz ruch do wykonania
+            if (aiMove != notFoundTuple) // jeśli masz jakiś
+            {
+                makeMove(buttonMap[aiMove.Item1, aiMove.Item2]); // wykonaj go jako gracz 2
+            }
+            else // jeśli nie masz żadnego, zakończ turę bez wykonywania ruchu
+            {
+                if (moveCounter >= 9)
+                {
+                    MessageBox.Show("REMIS");
+                    historyLabel.Text += "REMIS\n";
+                    resetButton.Enabled = true;
+                    resetButton.Visible = true;
+                    return;
+                }
+                player = !player;
+                label1.Text = "Teraz kolej gracza: " + playerString();
+            }
+        }
+
+        private int playerBonus(bool check)
+        {
+            return Convert.ToInt32(check == player);
+        }
+        private string playerString()
+        {
+            return (player ? "O" : "X");
+        }
+        private int playerInt()
+        {
+            return player ? 1 : -1;
+        }
+        private bool endGame()
+        {
+            return (LookForSums() != notFoundTuple); // szuka pełnych linii, jak nie dostanie notFoundTuple, to znaczy że ktoś wygrał
+        }
+        private Tuple<int, int> AICheckMoves()
+        {
+            Tuple<int, int> look = LookForSums(true); // szuka prawie dokończonych linii
+           if (look != notFoundTuple) // jeśli jakaś linia jest prawie dokończona (tj. albo zaraz wygra albo zaraz przegra)
+           {
+                return look; // zwraca kratkę gdzie jeszcze niczego nie ma
+           }
+           else if (gameState[1, 1] == 0) // jak nie, to sprawdza czy jest środek
+            {
+                return new Tuple<int, int>(1, 1); // jak jest to daje
+            }
+            else // jak nie, bierze pierwszą lepszą kratkę
+            {
+                for (int x = 0; x <= 2; x++)
+                {
+                    for (int y = 0; y <= 2; y++)
+                    {
+                        if (gameState[x, y] == 0)
+                        {
+                            Debug.WriteLine($"RANDOM: {x},{y}");
+                            return new Tuple<int, int>(x, y);
+                        }
+                    }
+                }
+            }
+            return notFoundTuple; // jak wystąpi jakiś bug gdzie nie ma żadnego ruchu do wykonania, zwraca notFoundTuple
+        }
+        private Tuple<int,int> LookForSums(bool ai=false) // sprawdza sumę danej linii - żeby się nie powtarzać pomiędzy funkcjami endGame() i AICheckMoves()
+        {
+            int sum = ai ? 2 : 3; // jeśli szukamy pod ai, szukamy linii z sumą 2, jeśli pod wygraną - 3
+            int sumRow = 0;
+            int sumCol = 0;
+            int sumDiagL = 0;
+            int sumDiagR = 0;
+            Tuple<int,int> def = new Tuple<int, int>(0, 0); // defaultowa wartość dla funkcji endGame()
+            for (int i = 0; i <= 2; i++)
+            {
+                sumRow = 0;
+                sumCol = 0;
+                for (int j = 0; j <= 2; j++)
+                {
+                    sumRow += gameState[i, j]; // sumuje rzędy i kolumny w tym samym czasie
+                    sumCol += gameState[j, i];
+                }
+                if (Math.Abs(sumRow) == sum)
+                {
+                    if (!ai) return def; // jeśli nie obchodzi nas czy mamy puste kratki w tym rzędzie - np. w funkcji endGame - zwracamy wartość defaultową by cokolwiek zwrócić
+                    for (int j = 0; j <= 2; j++)
+                    {
+                        if (gameState[i, j] == 0) // jeśli znajdzie pustą kratkę
+                        {
+                            return new Tuple<int, int>(i, j); // zwraca ją
+                        }
+                    }
+                }
+                if (Math.Abs(sumCol) == sum)
+                {
+                    if (!ai) return def;
+                    for (int j = 0; j <= 2; j++)
+                    {
+                        if (gameState[j, i] == 0)
+                        {
+                            return new Tuple<int, int>(j, i);
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i <= 2; i++)
+            {
+                sumDiagL += gameState[i, 2 - i];
+                sumDiagR += gameState[i, i];
+            }
+            if (Math.Abs(sumDiagR) == sum)
+            {
+                if (!ai) return def;
+                for (int j = 0; j <= 2; j++)
+                {
+                    if (gameState[j, j] == 0)
+                    {
+                        return new Tuple<int, int>(j, j);
+                    }
+                }
+            }
+            else if (Math.Abs(sumDiagL) == sum)
+            {
+                if (!ai) return def;
+                for (int j = 0; j <= 2; j++)
+                {
+                    if (gameState[j, 2 - j] == 0)
+                    {
+                        return new Tuple<int, int>(j, 2 - j);
+                    }
+                }
+            }
+            return notFoundTuple;
+        }
+        private void MessageGameState()
+        {
+            string output = "";
+            for (int i = 0; i <= 2; i++)
+            {
+                for (int j = 0; j <= 2; j++)
+                {
+                    output += gameState[j,i].ToString();
+
+                }
+                output += "-";
+            }
+            MessageBox.Show(output);
+        }
+        private Tuple<int, int> MapButton(Button get)
+        {
+            Tuple<int, int> output = notFoundTuple;
+            for (int i = 0; i <=2; i++) {
+                for (int j = 0; j <= 2; j++)
+                {
+                    if (buttonMap[i, j] != null)
+                    {
+                        if (buttonMap[i, j].Tag == get.Tag)
+                        {
+                            output = Tuple.Create(i, j);
+                        }
+                    }
+                }
+            }
+            return output;
+        }
+
+        private void resetGame(object sender, EventArgs e)
+        {
+            firstGame = false;
+            InitGame();
+        }
+    }
+
+}
