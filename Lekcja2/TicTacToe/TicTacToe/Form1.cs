79acea946642ebed04e65e41e494738becd79320
aridai-shi <jakubkaczma5@gmail.com>
2022-05-28 10:04:40 +0200

    Lekcja 3 - Firebase

--- Lekcja2/TicTacToe/TicTacToe/Form1.cs
+++ Lekcja2/TicTacToe/TicTacToe/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,14 +14,15 @@ namespace TicTacToe
     public partial class Form1 : Form
     {
         private bool player = true;
-        private int[,] gameState = new int[3, 3];
-        private Button[,] buttonMap = new Button[3, 3];
-        private readonly Tuple<int,int> notFoundTuple = new Tuple<int,int>(4,4);
+        private int[,] gameState = new int[gridSize, gridSize];
+        private Button[,] buttonMap = new Button[gridSize, gridSize];
+        private readonly Tuple<int,int> notFoundTuple = new Tuple<int,int>(gridSize+1, gridSize+1);
         private Tuple<int,int> winCounter = new Tuple<int, int>(0,0);
         private int moveCounter = 0;
         private int streak = 0;
         private bool lastWinner = false;
         private bool firstGame = true;
+        private const int gridSize = 3;
         public Form1()
         {
             InitializeComponent();
@@ -66,7 +68,7 @@ namespace TicTacToe
         }
         private void inputButtonClicked(object sender, EventArgs e)
         {
-            if (endGame() || (moveCounter >= 10)) return;
+            if (endGame()|| (moveCounter >= 10)) return;
             Button button = (sender as Button);
             Tuple<int, int> coords = MapButton(button);
             if (coords != notFoundTuple)
@@ -78,8 +80,8 @@ namespace TicTacToe
             historyLabel.Text += $"{playerString()}: ({coords.Item1},{coords.Item2}) \n";
             moveCounter++;
             if (endGame()){
-                MessageBox.Show("Wygrał gracz" + playerInt().ToString() );
-                historyLabel.Text += "Wygrał gracz" + playerInt().ToString() + "\n";
+                MessageBox.Show("Wygrał gracz " + (player ? "1" :"2"));
+                historyLabel.Text += "Wygrał gracz " + (player ? "1" : "2") + "\n";
                 winCounter = new Tuple<int, int>(winCounter.Item1 + playerBonus(true), winCounter.Item2 + playerBonus(false));
                 resetButton.Enabled = true;
                 resetButton.Visible = true;
@@ -105,27 +107,45 @@ namespace TicTacToe
         }
         private int playerInt()
         {
-            return (1 + Convert.ToInt32(player));
+            return (player ? 1 : -1);
         }
         private bool endGame()
         {
-            for (int i = 0; i <= 2; i++)
+            /// <summary>
+            /// Jako, że stan każdej komórki jest zapisywany jako liczba od -1 do 1 (0: nic, 1: kółko, -1: krzyżyk)
+            /// Kiedy zsumujemy wartości danej linii (tj. rzędu, kolumny lub skosu), suma zwycięskiej linii zawsze będzie miała albo minimalną albo maksymalną możliwą wartość
+            /// Np. Suma rzędu OXO = 2, ale XXX = -3 i OOO=3
+            /// Zatem wystarczy przeiterować przez wszystkie możliwe linie, aby odnaleźć sumę
+            /// To rozwiązanie jest prostsze w odczycie niż ify, a także może skalować, jeśli wprowadzilibyśmy inne tryby gry (np. 4x4, 5x5, itd.)
+            /// </summary>
+            
+            int sumDiagL = 0; // sumowanie wartości w lewym skosie
+            int sumDiagR = 0; // sumowanie wartości w prawym skosie
+            for (int i = 0; i <= gridSize-1; i++)
             {
-                if((gameState[i,0] == gameState[i,1])&&(gameState[i, 1] == gameState[i, 2]) && gameState[i, 0] != 0)
+                int sumCol = 0; // sumowanie wartości kolumn
+                int sumRow = 0; // sumowanie wartości rzędów
+                for (int j = 0; j <= gridSize - 1; j++)
+                {
+                    sumCol += gameState[i, j]; 
+                    sumRow += gameState[j,i];
+                }
+                if (Math.Abs(sumCol) == gridSize)
                 {
                     return true;
                 }
-                if ((gameState[0,i] == gameState[1,i]) && (gameState[1,i] == gameState[2,i]) && gameState[0,i] != 0)
+                if (Math.Abs(sumRow) == gridSize)
                 {
                     return true;
                 }
-                
+                sumDiagL += gameState[gridSize - i - 1, i];
+                sumDiagR += gameState[i, i];
             }
-            if ((gameState[0, 0] == gameState[1, 1]) && (gameState[1, 1] == gameState[2, 2]) && gameState[0, 0] != 0)
+            if (Math.Abs(sumDiagL) == gridSize)
             {
                 return true;
             }
-            if ((gameState[2, 0] == gameState[1, 1]) && (gameState[1, 1] == gameState[0, 2]) && gameState[2, 0] != 0)
+            if (Math.Abs(sumDiagR) == gridSize)
             {
                 return true;
             }
@@ -138,12 +158,12 @@ namespace TicTacToe
             {
                 for (int j = 0; j <= 2; j++)
                 {
-                    output += gameState[j,i].ToString();
+                    output += gameState[j,i].ToString().Replace("-1","-").Replace("1","+");
 
                 }
-                output += "-";
+                output += "/";
             }
-            MessageBox.Show(output);
+            Debug.WriteLine(output);
         }
         private Tuple<int, int> MapButton(Button get)
         {
---
+++ Lekcja3/Lekcja3/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+namespace Lekcja3
+{
+    public partial class Form1 : Form
+    {
+        private FirebaseAuthProvider prov;
+        private FirebaseAuthLink auth;
+        private FirebaseClient firebase;
+        private User user;
+        public Form1()
+        {
+            InitializeComponent();
+            prov = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyB6OLtr2nTsCE9QbjjzFh5ipQcogXQSscY"));
+            //prov.SignInWithEmailAndPasswordAsync("jakubkaczma5@gmail.com", "Asaasa12");
+            try{
+                auth = prov.SignInWithEmailAndPasswordAsync("jakubkaczma5@gmail.com", "Asaasa12").GetAwaiter().GetResult();
+            }
+            catch (FirebaseAuthException e)
+            {
+                var create = prov.CreateUserWithEmailAndPasswordAsync("jakubkaczma5@gmail.com", "Asaasa12").GetAwaiter().GetResult();
+                auth = prov.SignInWithEmailAndPasswordAsync("jakubkaczma5@gmail.com", "Asaasa12").GetAwaiter().GetResult();
+            }
+            firebase = new FirebaseClient("https://testproject-639b1-default-rtdb.europe-west1.firebasedatabase.app/",new FirebaseOptions{AuthTokenAsyncFactory = () => Task.FromResult(auth.FirebaseToken)});
+            if (firebase != null)
+            {
+                List<string> fromCloud = firebase.Child("lists").Child(auth.User.LocalId).OnceSingleAsync<List<string>>().GetAwaiter().GetResult();
+                if (fromCloud==null)
+                {
+                    firebase.Child("lists").Child(auth.User.LocalId).PutAsync(lines).GetAwaiter().GetResult();
+                }
+                else
+                {
+                    lines = fromCloud;
+                    updateLines();
+                };
+            }
+        }
+
+        private List<string> lines = new List<string>();
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char) Keys.Return && !string.IsNullOrEmpty(textBox1.Text))
+            {
+                lines.Add(textBox1.Text);
+                updateLines();
+                textBox1.Text = "";
+            }
+        }
+        private void updateLines()
+        {
+            textBox2.Lines = lines.ToArray();
+            Debug.WriteLine(textBox2.Lines);
+            firebase.Child("lists").Child(auth.User.LocalId).PutAsync(lines).GetAwaiter().GetResult();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
